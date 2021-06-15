ad5f419a301f0291ad473f80659ee63d81fd6130
TNTprizz <62872282+TNTprizz@users.noreply.github.com>
2021-06-15 21:19:40 +0800

    Additional modification

--- launch.py
+++ launch.py
@@ -1,3 +1,7 @@
 import os
+token = open("../E.key","r+")
+print("using token:" + token.read())
+token.close()
+print("Launching the bot, it shoud be online in less than 7 seconds.")
 os.chdir("./src")
 os.system("python3 bot.py")
